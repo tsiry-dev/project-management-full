894c2eb7744c8f419bf94ff70ad6d0e625a75666
Tsiry <andriamiarijaonatsiri@gmail.com>
2025-06-09 23:51:54 +0300

    RegisterControllerTest featured

---
+++ tests/Feature/Auth/RegisterControllerTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RegisterControllerTest extends TestCase
+{
+
+    public function test_register_response_contains_success_message()
+    {
+        $response = $this->postJson('/api/register', [
+            'name' => 'John Doe',
+            'email' => 'john@example.com',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ]);
+
+        $response->assertStatus(200)
+                ->assertJson([
+                    'message' => 'Votre compte a été créé avec succès',
+                ]);
+    }
+}
